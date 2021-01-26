9842cb5b3f11406b82f5cbc3cc5f20380d94aac3
Dotan Ganor <dotanganor10@gmail.com>
2021-01-26 18:28:22 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  display=false;
+
+  toggle() {
+  this.display = !this.display;
+
+  if(this.display)  
+    this.title = "";
+  else
+    this.title = 'Tour of Heroes'
+}
+}
